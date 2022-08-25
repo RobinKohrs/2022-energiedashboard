0491681aad773e8a58fae563db31dd1f8aec1a4d
RobinKohrs <robin.kohrs@gmx.de>
2022-08-25 17:03:54 +0200

    typo

--- R/get_weekly_diesel_prices.R
+++ R/get_weekly_diesel_prices.R
@@ -18,6 +18,6 @@ tomorrow = today + 1
 tdy = gsub("-", "_", today)
 tmr = gsub("-","_", tomorrow)
 
-fn = here(sprintf("output/weekly_fuel_prices/%s.csv", tdy))
+fn = here::here(sprintf("output/weekly_fuel_prices/%s.csv", tdy))
 dir = dirname(fn); if(!dir.exists(dir)) dir.create(dir)
 write.csv(data, fn)
